4ff9b1f875fa5ccd9a216e62d16426f6aa71f96d
Nicole Holder <nholder06@gmail.com>
2019-11-07 13:04:01 -0600

    updated launch status to reflect incorrect inputs

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
-   form.addEventListener("submit", function(event) {
-       let pilotName = document.querySelector("input[name = pilotName]") 
+   let pilotName = document.querySelector("input[name = pilotName]") 
        let coPilotName = document.querySelector("input[name = copilotName]")
        let fuelLevel = document.querySelector("input[name = fuelLevel]")
        let cargoMass = document.querySelector("input[name = cargoMass]")
+   form.addEventListener("submit", function(event) {
        if(pilotName.value === "" || coPilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "" 
        || pilotName.value === !String || coPilotName.value === !String || isNaN(fuelLevel.value) || isNaN(cargoMass.value) ){
       alert("All fields are required.");
       event.preventDefault();
        };
+      });
+      let submitButton = document.getElementById("formSubmit");
+      formSubmit.addEventListener("click", function(){
+
+         let pilotStatus = document.getElementById("pilotStatus");
+         pilotStatus.innerHTML = `${pilotName.value} is ready for launch.`
+         let coPilotStatus = document.getElementById("copilotStatus");
+         coPilotStatus.innerHTML = `${coPilotName.value} is ready for launch.`
+
+         if(fuelLevel.value < 10000){
+            faultyItems.style.visibility = "visible"
+            fuelStatus.innerHTML = `Not enough fuel for your journey!`
+            let launchStatus = document.getElementById("launchStatus");
+            launchStatus.innerHTML = `Shuttle not ready for launch.`
+            launchStatus.style.color = "red"
+         }else if(cargoMass.value > 10000){
+            cargoMass.innerHTML = `Too much mass for takeoff.`
+            let launchStatus = document.getElementById("launchStatus");
+            launchStatus.innerHTML = `Shuttle not ready for launch.`
+            launchStatus.style.color = "red"
+         }else{
+            faultyItems.style.visibility = "visible"
+            let launchStatus = document.getElementById("launchStatus");
+            launchStatus.innerHTML = `Shuttle is ready for takeoff.`
+            launchStatus.style.color = "green"
+         }    
+      });
        fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+                        let missionTarget = document.getElementById("missionTarget");
                         response.json().then(function(json) {
-                            let name = document.getElementById("name");
-                            name.innerHTML += `${json[2].name}`
-                            let diameter = document.getElementById("diameter");
-                            diameter.innerHTML += `${json[2].diameter}`
-                            let star = document.getElementById("star");
-                            star.innerHTML += `${json[2].star}`
-                            let distance = document.getElementById("distance");
-                            distance.innerHTML += `${json[2].distance}`
-                            let moons = document.getElementById("moons");
-                            moons.innerHTML += `${json[2].moons}`
-                            let img = document.getElementById("img");
-                            img.innerHTML = `${json[2].image}`
-                    });
+                           missionTarget.innerHTML = `
+                           <h2>Mission Destination</h2>
+                           <ol>
+                           <li>Name: ${json[2].name}</li>
+                           <li>Diameter: ${json[2].diameter}</li>
+                           <li>Star: ${json[2].star}</li>
+                           <li>Distance from Earth: ${json[2].distance}</li>
+                           <li>Number of Moons: ${json[2].moons}</li>
+                           </ol>
+                           <img src = "${json[2].image}">
+                           `
                 });
-    });           
-});
+    });
+});        
+   
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
@@ -41,5 +68,3 @@ window.addEventListener("load", function() {
 </ol>
 <img src="${}">
 */
-
-//window.onload = destination;
